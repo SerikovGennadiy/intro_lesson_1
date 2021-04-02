5302f0a2f06d9303f6bfc1ec05ec71f3d2416b5d
SerikovGennadiy <66972935+SerikovGennadiy@users.noreply.github.com>
2021-04-03 01:21:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,11 +9,8 @@ namespace lesson_1
             Console.WriteLine("Как тебя зовут?");
             string name = Console.ReadLine();
             Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now:D}");
-<<<<<<< HEAD
-
+            
             Console.ReadKey();
-=======
->>>>>>> e59b922eb01566107280ecc34fc38b9c786969fd
         }
     }
 }
