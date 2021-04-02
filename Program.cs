d309fa77a802ed4fb7f2de6738c60a2e48e69ac0
serikovgena <genahost@mail.ru>
2021-04-03 01:12:14 +0300

    feat: lesson_1 v1.2

--- Program.cs
+++ Program.cs
@@ -4,16 +4,13 @@ namespace lesson_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Как тебя зовут?");
             string name = Console.ReadLine();
             Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now:D}");
-<<<<<<< HEAD
 
             Console.ReadKey();
-=======
->>>>>>> e59b922eb01566107280ecc34fc38b9c786969fd
         }
     }
 }
